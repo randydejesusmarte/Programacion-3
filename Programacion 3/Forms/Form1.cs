c364bcedefbc872051a8b61a165d4165ce3fac47
randydejesusmarte <72465717+randydejesusmarte@users.noreply.github.com>
2021-01-12 23:19:15 -0400

    Agregar archivos de proyecto.

---
+++ Programacion 3/Forms/Form1.cs
@@ -0,0 +1,30 @@
+﻿using Programacion_3.Clases;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Programacion_3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        //[Atributo_Prueba("randy",22)]
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Atributo_Prueba atributo = new Atributo_Prueba();
+            atributo.Nombre = "Randy";
+            atributo.Edad = 22;
+            atributo.resultados();
+            MessageBox.Show(atributo.Resultado);
+        }
+    }
+}
